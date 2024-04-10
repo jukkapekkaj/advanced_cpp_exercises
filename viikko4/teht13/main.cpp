407c858fafd74c21b783d96f4679d62481027c8f
Jukka-Pekka Jaakkola <jukka-pekka.jaakkola@outlook.com>
2024-04-10 13:00:50 +0300

    create folders for new exercises

---
+++ viikko4/teht13/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+
+int main(void)   {
+    std::cout << "hello" << std::endl;
+    std::string s1("kissa");
+    std::string s2;
+    int n = 10;
+
+    s1 = s2;
+
+    std::cout << "done" << std::endl;
+
+
+
+
+    return 0;
+}
